a6e6eef5852ae77c172de899019f44cb4f579b15
Teodor Bjerre <teodor.bjerre@gmail.com>
2019-01-29 15:08:48 +0000

    Updates

--- precache-manifest.aa094efee1e676c0b9bc767877f436e9.js
+++ precache-manifest.aa094efee1e676c0b9bc767877f436e9.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
   },
   {
-    "revision": "ca1268723e23257e1e9f",
-    "url": "/robofriends/static/js/main.ca126872.chunk.js"
+    "revision": "64904dadd8f4d41afe10",
+    "url": "/robofriends/static/js/main.64904dad.chunk.js"
   },
   {
     "revision": "b0860ea33a3546789c6e",
     "url": "/robofriends/static/js/1.b0860ea3.chunk.js"
   },
   {
-    "revision": "ca1268723e23257e1e9f",
-    "url": "/robofriends/static/css/main.f4f21b03.chunk.css"
+    "revision": "64904dadd8f4d41afe10",
+    "url": "/robofriends/static/css/main.8db9b13f.chunk.css"
   },
   {
     "revision": "b0860ea33a3546789c6e",
     "url": "/robofriends/static/css/1.e3f447e5.chunk.css"
   },
   {
-    "revision": "d111327470c25fba59e7ce8b8719e0c5",
+    "revision": "571302279c0680df8f8c155547d30e27",
     "url": "/robofriends/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.cd2e800690f2ca59bcf187821faebce2.js"
+  "/robofriends/precache-manifest.aa094efee1e676c0b9bc767877f436e9.js"
 );
 
 workbox.clientsClaim();
