10262495a2ee95ec95e10160bb29313928873271
Teodor Bjerre <teodor.bjerre@gmail.com>
2019-01-29 14:02:16 +0000

    Updates

---
+++ precache-manifest.cd2e800690f2ca59bcf187821faebce2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "ca1268723e23257e1e9f",
+    "url": "/robofriends/static/js/main.ca126872.chunk.js"
+  },
+  {
+    "revision": "b0860ea33a3546789c6e",
+    "url": "/robofriends/static/js/1.b0860ea3.chunk.js"
+  },
+  {
+    "revision": "ca1268723e23257e1e9f",
+    "url": "/robofriends/static/css/main.f4f21b03.chunk.css"
+  },
+  {
+    "revision": "b0860ea33a3546789c6e",
+    "url": "/robofriends/static/css/1.e3f447e5.chunk.css"
+  },
+  {
+    "revision": "d111327470c25fba59e7ce8b8719e0c5",
+    "url": "/robofriends/index.html"
+  }
+];
