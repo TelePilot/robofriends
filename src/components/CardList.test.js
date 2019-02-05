1a7a89d17308966e3843ed09334832c8c576c325
Teodor Bjerre <teodor.bjerre@gmail.com>
2019-02-05 16:43:46 +0000

    added test and snapshots

---
+++ src/components/CardList.test.js
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import CardList from './CardList';
+
+it('expect to render CardList', () => {
+  const mockRobots = [
+    {
+      id: 1,
+      name: 'John bajs',
+      username: 'Jonnie',
+      email: 'john@bajs.com'
+    }
+  ]
+  expect(shallow(<CardList robots={mockRobots}/>)).toMatchSnapshot();
+
+})
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ class Header extends Component {
     return false
   }
   render() {
-    console.log('Header')
     return (
       <div>
 
